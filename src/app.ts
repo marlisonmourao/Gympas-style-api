02c0e4cef087f553aee0ac180260a2de646f7ba8
marlisonmourao <bentesmourao@gmail.com>
2023-09-23 22:10:21 -0400

    config docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+export const prisma = new PrismaClient()
+prisma.user.create({
+  data: {
+    name: 'Marlison Bentes Mourão',
+    email: 'bentesmourao@gmail.com',
+  },
+})
