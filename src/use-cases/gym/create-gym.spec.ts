4dc932548e13fb0df73773826f5bc3e31bc821af
marlisonmourao <bentesmourao@gmail.com>
2023-10-23 18:48:46 -0400

    feat: create  gym

---
+++ src/use-cases/gym/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymReposity } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymReposity
+let sut: GymUseCase
+
+describe('Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymReposity()
+    sut = new GymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Javascript Gym',
+      description: 'teste',
+      latitude: -3.0737507,
+      longitude: -59.9219333,
+      phone: null,
+    })
+
+    await expect(gym.id).toEqual(expect.any(String))
+  })
+})
