bb02d057c5f121577f4293d3d05509125d1adf64
marlisonmourao <bentesmourao@gmail.com>
2023-11-03 19:23:10 -0400

    fix path

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,7 @@
 import { verifyJWT } from '@/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate/authenticate.controller'
-import { profile } from './controllers/profile'
+import { profile } from './controllers/profile/profile.controller'
 import { register } from './controllers/register/register.controller'
 
 export async function appRoutes(app: FastifyInstance) {
