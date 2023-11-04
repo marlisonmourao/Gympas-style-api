8af2907e4ad49850f32ea01b623c1c14d610763c
marlisonmourao <bentesmourao@gmail.com>
2023-11-04 13:17:42 -0400

    feat: refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,11 +2,14 @@ import { verifyJWT } from '@/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './authenticate/authenticate.controller'
 import { profile } from './profile/profile.controller'
+import { refresh } from './refresh/refresh.controller'
 import { register } from './register/register.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
